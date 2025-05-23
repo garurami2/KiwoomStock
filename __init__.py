e894471cbb668822fdb6b260aad2f2597c1adb20
garurami1 <garurami1@naver.com>
2025-05-23 19:09:40 +0900

    로그 추가

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
-from kiwoom.kiwoom import *
 import sys
+
+from kiwoom.kiwoom import *
 from PyQt5.QtWidgets import *
+from config.log_class import *
 
 
 class Main():
     def __init__(self):
-        print("Main() start")
+        self.logging = Logging()
+
+        self.logging.logger.debug("Main() start")
         self.app = QApplication(sys.argv) # PyQt5로 실행할 파일명을 자동 설정
         self.kiwoom = Kiwoom() # 키움 클래스 객체화
         self.app.exec_() # 이벤트 루프 실행
